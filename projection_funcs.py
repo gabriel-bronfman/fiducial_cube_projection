6656ef0ceee0570fd169202b12ec2256253babab
Gabriel Bronfman <gabrielbronfman@MacBook-Pro-8.local>
2023-10-06 15:09:38 -0400

    First Commit

---
+++ projection_funcs.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cv2
+
+def homography(input_pts, output_pts):
+    
+    A = np.array([], dtype=np.int64).reshape(0,9)
+    
+    for (ptW,ptI) in zip(input_pts,output_pts):
+        
+        [x,y,_] = ptW
+        [xp,yp] = ptI
+        
+        #Contruct Ax=0 equation to solve via Singular Value Decomp
+        A = np.vstack([A,np.array([[-x, -y, -1, 0, 0, 0, x*xp, y*xp, xp], 
+                                   [0, 0, 0, -x, -y, -1, x*yp, y*yp, yp]])])
+    
+    
+    U, Sigma, V = np.linalg.svd(A)
+    
+    h = np.reshape(V[-1, :], (3, 3))
+    
+    #After extracting the information, we normalize by h[3,3] to make h[3,3] 1
+    s = h[-1,-1]
+    H = h/s
+    
+    return h
+
+def solveExtrinsicsFromH(h,k):
+    b = (np.linalg.inv(k)@h)
+    
+    if np.linalg.det(b)<0:
+        b =  b*(-1)
+    [a1,a2,a3] = b.T
+    
+    #Instead of normalizing by the value of A1, or A2, we average the two and normalize by both to create the better rotation 
+    # matrix
+    Lambda = 2/(np.linalg.norm(a1) + np.linalg.norm(a2))
+    r1 = Lambda * a1
+    r2 = Lambda * a2
+    r3 = np.cross(r1, r2, axis = 0)
+    t = Lambda * a3
+    h =  np.column_stack((r1, r2, r3))
+     
+    return h,t
+
+def drawCube(frame,tag_corners, cube_corners):
+    (CubePtA,CubePtB,CubePtC,CubePtD) = cube_corners
+    (ptA, ptB, ptC, ptD) = tag_corners
+    
+    ptB = (int(ptB[0]), int(ptB[1]))
+    ptC = (int(ptC[0]), int(ptC[1]))
+    ptD = (int(ptD[0]), int(ptD[1]))
+    ptA = (int(ptA[0]), int(ptA[1]))
+  
+    CubePtA = (int(CubePtA[0][0]), int(CubePtA[0][1]))
+    CubePtB = (int(CubePtB[0][0]), int(CubePtB[0][1]))
+    CubePtC = (int(CubePtC[0][0]), int(CubePtC[0][1]))
+    CubePtD = (int(CubePtD[0][0]), int(CubePtD[0][1]))
+
+    
+    cv2.circle(frame, ptA, 20, (0, 0, 255), -1)
+    cv2.circle(frame, ptB, 20, (0, 255, 0), -1)
+    cv2.circle(frame, ptC, 20, (255, 0, 0), -1)
+    cv2.circle(frame, ptD, 20, (255, 255, 255), -1)
+    
+    cv2.circle(frame, CubePtA, 20, (0, 0, 255), -1)
+    cv2.circle(frame, CubePtB, 20, (0, 255, 0), -1)
+    cv2.circle(frame, CubePtC, 20, (255, 0, 0), -1)
+    cv2.circle(frame, CubePtD, 20, (255, 255, 255), -1)
+
+    cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+    cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
+    cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
+    cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
+
+    cv2.line(frame, CubePtA, CubePtB, (0, 255, 0), 2)
+    cv2.line(frame, CubePtB, CubePtC, (0, 255, 0), 2)
+    cv2.line(frame, CubePtC, CubePtD, (0, 255, 0), 2)
+    cv2.line(frame, CubePtD, CubePtA, (0, 255, 0), 2)
+
+    cv2.line(frame, CubePtA, ptA, (0, 255, 0), 2)
+    cv2.line(frame, CubePtB, ptB, (0, 255, 0), 2)
+    cv2.line(frame, CubePtC, ptC, (0, 255, 0), 2)
+    cv2.line(frame, CubePtD, ptD, (0, 255, 0), 2)
+    
+    return frame
+
+    
+
+    
+    
---
+++ unoptimized_projection.py
@@ -0,0 +1,68 @@
+import cv2
+import apriltag
+import numpy as np
+from projection_funcs import homography, solveExtrinsicsFromH, drawCube
+
+cam = cv2.VideoCapture(1)
+
+#Detector for class "36h11" AprilTags, can be changed for any type of AprilTag
+options = apriltag.DetectorOptions(families="tag36h11")
+detector = apriltag.Detector(options)
+
+#K matrix for my own camera, change for your own camera, my camera has no radial distortion effects but they can be added
+k = np.array([[2063.8820747760683, 0, 1184.6568232403756],
+             [0, 2060.440690811184, 763.9115119061984],
+             [0,0,1]])
+dist_coeffs = np.zeros((5, 1), np.float32)
+
+#Define corners of the tag in the global frame. Units don't matter as all distances will be defined relative to the perceived size of the AprilTag, so we use 1 for convenience
+corner_tag_global_frame = np.array([[0,0,0],[0,1,0],[1,1,0],[1,0,0]],dtype = np.float64)
+corner_tag_pixel = []
+
+if not cam.isOpened():
+    print("Fail")
+    exit(0)
+
+
+while True:
+    ret, frame = cam.read()
+    
+    if not ret:
+        print("Failed while live")
+        break
+    
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+        
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    results = detector.detect(grey)
+    
+    for r in results:
+        #Extract Corners
+        corner_tag_pixel = r.corners
+        
+        #Solve homography for seen tag, then extract camera extrinsic parameters from seen homography
+        h = homography(corner_tag_global_frame,corner_tag_pixel)
+        rotation,tvec = solveExtrinsicsFromH(h,k)
+        
+        #Convert rotation matrix to Angle-Axis representation via Rodrigues formula
+        rvec,_ = cv2.Rodrigues(rotation)
+        
+        #Project points into image plane as if there was an imaged cube above the april tag using
+        # [u,v] = K[R|t][X,Y,Z]
+        cube_points,_ = cv2.projectPoints(np.array([[0,0,-1],[0,1,-1],[1,1,-1],[1,0,-1]],
+                                                                         dtype =np.float64), rvec,tvec,k,dist_coeffs)
+        #Draw Cube
+        frame = drawCube(frame,corner_tag_pixel,cube_points)
+        
+        # draw the center (x, y)-coordinates of the AprilTag
+        (cX, cY) = (int(r.center[0]), int(r.center[1]))
+        cv2.circle(frame, (cX, cY), 5, (0, 0, 255), -1)
+    
+ 
+    cv2.imshow("video", frame)
+cam.release()
+cv2.destroyAllWindows()
+        
